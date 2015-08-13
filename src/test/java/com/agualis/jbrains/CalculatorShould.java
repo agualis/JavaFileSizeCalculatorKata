02d44a8701de0c1865d1340ed56e02ddb62eafe4
Alberto Gualis <agualis@gmail.com>
2015-08-13 17:18:38 +0200

    Trying to launch cobertura in shippable2

--- src/test/java/com/agualis/jbrains/CalculatorShould.java
+++ src/test/java/com/agualis/jbrains/CalculatorShould.java
@@ -5,9 +5,6 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
-/**
- * Created by agualis on 13/8/15.
- */
 public class CalculatorShould {
 
     private Calculator calculator;
@@ -17,12 +14,5 @@ public class CalculatorShould {
     add_two_numbers() {
         calculator = new Calculator();
         assertThat(calculator.add(2, 3), is(5));
-    }
-
-    @Test
-    public void
-    add_two_numbers_invalid() {
-        calculator = new Calculator();
-        assertThat(calculator.add(2, 3), is(4));
     }
 }
