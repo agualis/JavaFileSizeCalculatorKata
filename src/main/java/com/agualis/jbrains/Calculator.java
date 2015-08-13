d2bf265faee8bf291c92f75049e983a10c8d0abc
Alberto Gualis <agualis@gmail.com>
2015-08-13 16:51:45 +0200

    First demo test

---
+++ src/main/java/com/agualis/jbrains/Calculator.java
@@ -0,0 +1,10 @@
+package com.agualis.jbrains;
+
+/**
+ * Created by agualis on 13/8/15.
+ */
+public class Calculator {
+    public int add(int first, int second) {
+        return first + second;
+    }
+}
---
+++ src/test/java/com/agualis/jbrains/CalculatorShould.java
@@ -0,0 +1,21 @@
+package com.agualis.jbrains;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by agualis on 13/8/15.
+ */
+public class CalculatorShould {
+
+    private Calculator calculator;
+
+    @Test
+    public void
+    add_two_numbers() {
+        calculator = new Calculator();
+        assertThat(calculator.add(2, 3), is(5));
+    }
+}
